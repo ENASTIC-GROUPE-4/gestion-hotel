5122d2b634184142087d25ff28d9690667b128fe
idrissmai6661µ <imidlol1994@gmail.com>
2021-07-12 21:23:23 +0100

    initial commit

---
+++ src/main/java/com/gestionhotel/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.gestionhotel.web.rest.vm;
---
+++ src/test/java/com/gestionhotel/ArchTest.java
@@ -0,0 +1,29 @@
+package com.gestionhotel;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.gestionhotel");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.gestionhotel.service..")
+            .or()
+            .resideInAnyPackage("com.gestionhotel.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.gestionhotel.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
